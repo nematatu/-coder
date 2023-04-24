cc4fe2a011e108954134c81a3649fd976e310f42
syungite <earthwithheart@gmail.com>
2023-04-24 12:41:20 +0900

    UIの追加１です

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:dart_openai/openai.dart';
 import './next_page.dart';
 import './battle_page.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
       ),
       home: MyHomePage(
         title: 'test_code',
@@ -41,36 +43,75 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            const Text(
-              'test',
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.w500,
-                fontFamily: ('Roboto'),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/sakura.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset(
+                'images/karutalogo.png',
+                width: 500,
+                height: 500,
               ),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.blue,
-                  elevation: 10,
-                  fixedSize: Size(200, 60),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50))),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Nextpage()),
-                );
-              },
-              child: Text('スタート'),
-            ),
-          ],
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.pink,
+                    elevation: 10,
+                    fixedSize: Size(200, 60),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50))),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Nextpage()),
+                  );
+                },
+                child: Text('スタート'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           children: <Widget>[
+//             const Text(
+//               'test',
+//               style: TextStyle(
+//                 fontSize: 50,
+//                 fontWeight: FontWeight.w500,
+//                 fontFamily: ('Roboto'),
+//               ),
+//             ),
+//             ElevatedButton(
+//               style: ElevatedButton.styleFrom(
+//                   primary: Colors.blue,
+//                   elevation: 10,
+//                   fixedSize: Size(200, 60),
+//                   shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(50))),
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => Nextpage()),
+//                 );
+//               },
+//               child: Text('スタート'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/next_page.dart
+++ lib/next_page.dart
@@ -4,68 +4,174 @@ import 'battle_page.dart';
 import 'document_page.dart';
 
 class Nextpage extends StatelessWidget {
-  build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('test_code'),
       ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            OutlinedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => battle_page()),
-                );
-              },
-              style: OutlinedButton.styleFrom(
-                primary: Colors.blue,
-                fixedSize: Size(150, 200),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              child: Text(
-                '対戦',
-                style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.normal,
-                  fontFamily: 'Roboto',
-                ),
-              ),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => document_page(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background_tatami2.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => battle_page()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green[800],
+                  fixedSize: Size(150, 200),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                );
-              },
-              style: OutlinedButton.styleFrom(
-                primary: Colors.blue,
-                fixedSize: Size(150, 200),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  '対戦',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.normal,
+                    fontFamily: 'Roboto',
+                  ),
                 ),
               ),
-              child: Text(
-                '資料',
-                style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.normal,
-                  fontFamily: 'Roboto',
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => document_page(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  // shape: RoundedRectangleBorder(
+                  //   borderRadius: BorderRadius.circular(20),
+                  // ),
+                  padding: EdgeInsets.all(0.0),
+                  elevation: 0,
+                  primary: Colors.green,
                 ),
-              ),
-            ),
-          ],
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('images/karuta.png'),
+                      fit: BoxFit.cover,
+                    ),
+                    // borderRadius: BorderRadius.circular(20),
+                  ),
+                  height: 200,
+                  width: 150,
+                  child: Center(
+                    child: Text(
+                      '札一覧',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.normal,
+                        fontFamily: 'Roboto',
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+// class Nextpage extends StatelessWidget {
+//   build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('test_code'),
+//       ),
+//       body: Center(
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           mainAxisSize: MainAxisSize.max,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: <Widget>[
+//             OutlinedButton(
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(builder: (context) => battle_page()),
+//                 );
+//               },
+//               style: OutlinedButton.styleFrom(
+//                 primary: Colors.green[800],
+//                 fixedSize: Size(150, 200),
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(20),
+//                 ),
+//               ),
+//               child: Text(
+//                 '対戦',
+//                 style: TextStyle(
+//                   fontSize: 50,
+//                   fontWeight: FontWeight.normal,
+//                   fontFamily: 'Roboto',
+//                 ),
+//               ),
+//             ),
+//             ElevatedButton(
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                     builder: (context) => document_page(),
+//                   ),
+//                 );
+//               },
+//               style: ElevatedButton.styleFrom(
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(20),
+//                 ),
+//                 padding: EdgeInsets.all(0.0),
+//                 elevation: 0,
+//                 primary: Colors.transparent,
+//               ),
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                   image: DecorationImage(
+//                     image: AssetImage('images/ougi.png'),
+//                     fit: BoxFit.cover,
+//                   ),
+//                   borderRadius: BorderRadius.circular(20),
+//                 ),
+//                 height: 200,
+//                 width: 150,
+//                 child: Center(
+//                   child: Text(
+//                     '資料',
+//                     style: TextStyle(
+//                       fontSize: 50,
+//                       fontWeight: FontWeight.normal,
+//                       fontFamily: 'Roboto',
+//                       color: Colors.white,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
